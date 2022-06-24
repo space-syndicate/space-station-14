9864bae192e4c8b150c96a8dac6df6dfe529353b
Arthur <14136326+Morb0@users.noreply.github.com>
2022-06-24 04:33:24 +0300

    Add basic server icarus components

---
+++ Content.Server/Icarus/IcarusTerminalComponent.cs
@@ -0,0 +1,16 @@
+﻿namespace Content.Server.Icarus;
+
+/// <summary>
+/// Used to store Icarus terminal keys
+/// </summary>
+[RegisterComponent]
+public sealed class IcarusTerminalComponent : Component
+{
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("totalKeys")]
+    public int TotalKeys = 3;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("activatedKeys")]
+    public int ActivatedKeys = 0;
+}
---
+++ Content.Server/Icarus/IcarusTerminalSystem.cs
@@ -0,0 +1,28 @@
+﻿namespace Content.Server.Icarus;
+
+/// <summary>
+/// Handle Icarus activation terminal
+/// </summary>
+public sealed class IcarusTerminalSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+
+    }
+
+    public void OnKeyInsert()
+    {
+
+    }
+
+    public void Fire()
+    {
+        // TODO: Fire Icarus beam
+    }
+
+    public bool IsUnlocked(IcarusTerminalComponent comp)
+    {
+        return comp.ActivatedKeys == comp.TotalKeys;
+    }
+}
