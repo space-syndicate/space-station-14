a99c5c348883a971361b0aff061ffa620cf6f36c
Arthur <14136326+Morb0@users.noreply.github.com>
2022-06-26 06:39:24 +0300

    Move system to server and add basic fire logic

--- Content.Client/Icarus/IcarusTerminalSystem.cs
+++
@@ -1,8 +0,0 @@
-﻿using Content.Shared.Icarus;
-
-namespace Content.Client.Icarus;
-
-public sealed class IcarusTerminalSystem : SharedIcarusTerminalSystem
-{
-    // Run system on client for sounds on user client that insert keys
-}
--- Content.Server/Icarus/IcarusTerminalSystem.cs
+++ Content.Server/Icarus/IcarusTerminalSystem.cs
@@ -1,5 +1,42 @@
-﻿using Content.Shared.Icarus;
+﻿using System.Linq;
+using Content.Server.Chat.Systems;
+using Content.Shared.Containers.ItemSlots;
+using Content.Shared.Icarus;
+using Robust.Shared.Audio;
+using Robust.Shared.Player;
 
 namespace Content.Server.Icarus;
 
-public sealed class IcarusTerminalSystem : SharedIcarusTerminalSystem {}
+/// <summary>
+/// Handle Icarus activation terminal
+/// </summary>
+public sealed class IcarusTerminalSystem : EntitySystem
+{
+    [Dependency] private readonly ChatSystem _chatSystem = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<IcarusTerminalComponent, ItemSlotChangedEvent>(OnItemSlotChanged);
+    }
+
+    private void OnItemSlotChanged(EntityUid uid, IcarusTerminalComponent component, ref ItemSlotChangedEvent args)
+    {
+        if (IsUnlocked(uid))
+        {
+            Fire(uid);
+        }
+    }
+
+    public void Fire(EntityUid uid)
+    {
+        _chatSystem.DispatchStationAnnouncement(uid, "U AL' DED", "TODO", false, Color.Red);
+        SoundSystem.Play("/Audio/Corvax/AssaultOperatives/icarus_alarm.ogg", Filter.Broadcast());
+
+        // TODO: Fire Icarus beam
+    }
+
+    public bool IsUnlocked(EntityUid uid)
+    {
+        return EntityManager.GetComponent<ItemSlotsComponent>(uid).Slots.Values.All(v => v.HasItem);
+    }
+}
