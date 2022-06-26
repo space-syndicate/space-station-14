4591833d81a9350d5f7f10c4915ea2c5584bdd0a
Arthur <14136326+Morb0@users.noreply.github.com>
2022-06-26 07:16:52 +0300

    Add announcements & activation verb

--- Content.Server/Icarus/IcarusTerminalSystem.cs
+++ Content.Server/Icarus/IcarusTerminalSystem.cs
@@ -2,6 +2,7 @@
 using Content.Server.Chat.Systems;
 using Content.Shared.Containers.ItemSlots;
 using Content.Shared.Icarus;
+using Content.Shared.Verbs;
 using Robust.Shared.Audio;
 using Robust.Shared.Player;
 
@@ -17,19 +18,32 @@ public sealed class IcarusTerminalSystem : EntitySystem
     public override void Initialize()
     {
         SubscribeLocalEvent<IcarusTerminalComponent, ItemSlotChangedEvent>(OnItemSlotChanged);
+        SubscribeLocalEvent<IcarusTerminalComponent, GetVerbsEvent<AlternativeVerb>>(AddFireVerb);
     }
 
     private void OnItemSlotChanged(EntityUid uid, IcarusTerminalComponent component, ref ItemSlotChangedEvent args)
     {
         if (IsUnlocked(uid))
         {
-            Fire(uid);
+            SoundSystem.Play("/Audio/Machines/Nuke/confirm_beep.ogg", Filter.Pvs(uid), uid);
+
+            _chatSystem.DispatchStationAnnouncement(uid, Loc.GetString("goldeneye-authorized-announcement"), playDefaultSound: false);
+            SoundSystem.Play("/Audio/Misc/notice1.ogg", Filter.Broadcast()); // TODO: Just pass custom sound path after PR accepting
         }
+    }
+
+    private void AddFireVerb(EntityUid uid, IcarusTerminalComponent itemSlots, GetVerbsEvent<AlternativeVerb> args)
+    {
+        AlternativeVerb verb = new();
+        verb.Act = () => Fire(uid);
+        verb.Text = Loc.GetString("goldeneye-ui-fire");
+        verb.Priority = 0;
+        args.Verbs.Add(verb);
     }
 
     public void Fire(EntityUid uid)
     {
-        _chatSystem.DispatchStationAnnouncement(uid, "U AL' DED", "TODO", false, Color.Red);
+        _chatSystem.DispatchStationAnnouncement(uid, Loc.GetString("goldeneye-icarus-announcement"), Loc.GetString("goldeneye-announce-sender"), false, Color.Red);
         SoundSystem.Play("/Audio/Corvax/AssaultOperatives/icarus_alarm.ogg", Filter.Broadcast());
 
         // TODO: Fire Icarus beam
---
+++ Resources/Locale/ru-RU/corvax/assault-operatives/icarus-terminal-component.ftl
@@ -0,0 +1,10 @@
+﻿goldeneye-announce-sender = Icarus Defence Network
+goldeneye-authorized-announcement = UNAUTHORISED KEYCARD UPLOAD DETECTED. ALL KEYCARDS UPLOADED!
+goldeneye-icarus-announcement = /// GOLDENEYE DEFENCE NETWORK BREACHED ///
+              	Unauthorised GoldenEye Defence Network access detected.
+              	ICARUS online.
+              	Targeting system override detected...
+              	New target: /NTSS14/
+              	ICARUS firing protocols activated.
+              	ETA to fire: 20 seconds.
+goldeneye-ui-fire = Fire
