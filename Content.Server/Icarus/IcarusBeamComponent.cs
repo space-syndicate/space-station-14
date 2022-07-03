c0794feada76b34057cb1a209771f23b975be77b
Arthur <14136326+Morb0@users.noreply.github.com>
2022-07-03 16:40:03 +0300

    Basic Icarus beam

---
+++ Content.Server/Icarus/IcarusBeamComponent.cs
@@ -0,0 +1,16 @@
+﻿using Content.Shared.Sound;
+
+namespace Content.Server.Icarus;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed class IcarusBeamComponent : Component
+{
+    [DataField("loopSound")]
+    public SoundSpecifier Sound = new SoundPathSpecifier("/Audio/Corvax/AssaultOperatives/loop.ogg");
+
+    [DataField("speed")]
+    public float Speed = 1f;
+}
---
+++ Content.Server/Icarus/IcarusBeamSystem.cs
@@ -0,0 +1,29 @@
+﻿namespace Content.Server.Icarus;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class IcarusBeamSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<IcarusBeamComponent, ComponentInit>(OnComponentInit);
+    }
+
+    private void OnComponentInit(EntityUid uid, IcarusBeamComponent component, ComponentInit args)
+    {
+        if (EntityManager.TryGetComponent(uid, out PhysicsComponent? phys))
+        {
+            phys.LinearDamping = 0f;
+            phys.Friction = 0f;
+            phys.BodyStatus = BodyStatus.InAir;
+
+            var xform = Transform(uid);
+            var vel = new Vector2(component.Speed, 0);
+
+            phys.ApplyLinearImpulse(vel);
+            xform.LocalRotation = (vel - xform.WorldPosition).ToWorldAngle() + MathHelper.PiOver2;
+        }
+    }
+}
