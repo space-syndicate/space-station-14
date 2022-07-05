4793eaa49418e412e9c0e3c132f9b7976966707b
Arthur <14136326+Morb0@users.noreply.github.com>
2022-07-06 00:34:35 +0300

    Copy singularity destroy logic to beam

--- Content.Server/Icarus/IcarusBeamComponent.cs
+++ Content.Server/Icarus/IcarusBeamComponent.cs
@@ -8,6 +8,27 @@ public sealed class IcarusBeamComponent : Component
     [DataField("loopSound")]
     public SoundSpecifier Sound = new SoundPathSpecifier("/Audio/Corvax/AssaultOperatives/sunbeam_loop.ogg");
 
+    /// <summary>
+    ///     Beam moving speed.
+    /// </summary>
     [DataField("speed")]
     public float Speed = 1f;
+
+    /// <summary>
+    ///     The beam will be automatically cleaned up after this time.
+    /// </summary>
+    [DataField("lifetime")]
+    public TimeSpan Lifetime = TimeSpan.FromSeconds(240);
+
+    /// <summary>
+    ///     With this set to true, beam will automatically set the tiles under them to space.
+    /// </summary>
+    [DataField("destroyTiles")]
+    public bool DestroyTiles = false;
+
+    [DataField("destroyRadius")]
+    public float DestroyRadius = 2f;
+
+    [DataField("accumulator")]
+    public float Accumulator = 0f;
 }
--- Content.Server/Icarus/IcarusBeamSystem.cs
+++ Content.Server/Icarus/IcarusBeamSystem.cs
@@ -1,17 +1,34 @@
-﻿using Content.Server.Body.Components;
+﻿using Content.Server.Ghost.Components;
 using Robust.Shared.Audio;
-using Robust.Shared.Physics.Dynamics;
+using Robust.Shared.Map;
 using Robust.Shared.Player;
 
 namespace Content.Server.Icarus;
 
 public sealed class IcarusBeamSystem : EntitySystem
 {
-    /// <inheritdoc/>
+    [Dependency] private readonly IMapManager _map = default!;
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        foreach (var (beam, trans) in EntityManager.EntityQuery<IcarusBeamComponent, TransformComponent>(true))
+        {
+            AccumulateLifetime(beam, frameTime);
+            DestroyEntities(beam, trans);
+
+            if (!beam.DestroyTiles)
+                continue;
+
+            DestroyTiles(beam, trans);
+        }
+    }
+
     public override void Initialize()
     {
         SubscribeLocalEvent<IcarusBeamComponent, ComponentInit>(OnComponentInit);
-        SubscribeLocalEvent<IcarusBeamComponent, StartCollideEvent>(OnCollide);
     }
 
     private void OnComponentInit(EntityUid uid, IcarusBeamComponent component, ComponentInit args)
@@ -32,14 +49,62 @@ public sealed class IcarusBeamSystem : EntitySystem
         SoundSystem.Play(component.Sound.GetSound(), Filter.Pvs(uid), uid, AudioParams.Default.WithLoop(true));
     }
 
-    private void OnCollide(EntityUid uid, IcarusBeamComponent component, StartCollideEvent args)
+    private void AccumulateLifetime(IcarusBeamComponent beam, float frameTime)
     {
-        var ent = args.OtherFixture.Body.Owner;
+        beam.Accumulator += frameTime;
+        if (beam.Accumulator > beam.Lifetime.TotalSeconds)
+        {
+            QueueDel(beam.Owner);
+        }
+    }
 
-        // Gib everyone
-        if (TryComp<BodyComponent>(ent, out var body))
-            body.Gib();
+    /// <summary>
+    /// Destroy any grid tiles in beam radius.
+    /// </summary>
+    private void DestroyTiles(IcarusBeamComponent component, TransformComponent trans)
+    {
+        var radius = component.DestroyRadius;
+        var worldPos = trans.WorldPosition;
 
-        QueueDel(ent);
+        var circle = new Circle(worldPos, radius);
+        var box = new Box2(worldPos - radius, worldPos + radius);
+
+        foreach (var grid in _map.FindGridsIntersecting(trans.MapID, box))
+        {
+            // Bundle these together so we can use the faster helper to set tiles.
+            var toDestroy = new List<(Vector2i, Tile)>();
+
+            foreach (var tile in grid.GetTilesIntersecting(circle))
+            {
+                if (tile.Tile.IsEmpty)
+                    continue;
+
+                toDestroy.Add((tile.GridIndices, Tile.Empty));
+            }
+
+            grid.SetTiles(toDestroy);
+        }
+    }
+
+    /// <summary>
+    /// Handle deleting entities in beam radius.
+    /// </summary>
+    private void DestroyEntities(IcarusBeamComponent component, TransformComponent trans)
+    {
+        var radius = component.DestroyRadius - 0.5f;
+        foreach (var entity in _lookup.GetEntitiesInRange(trans.MapID, trans.WorldPosition, radius))
+        {
+            if (!CanDestroy(component, entity))
+                continue;
+
+            QueueDel(entity);
+        }
+    }
+
+    private bool CanDestroy(IcarusBeamComponent component, EntityUid entity)
+    {
+        return entity != component.Owner &&
+               !EntityManager.HasComponent<IMapGridComponent>(entity) &&
+               !EntityManager.HasComponent<GhostComponent>(entity);
     }
 }
