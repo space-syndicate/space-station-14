acc2fd5a558023c07e40504f86581b22629d89b8
Arthur <14136326+Morb0@users.noreply.github.com>
2022-07-11 02:15:30 +0300

    Start creating spawn beam command

---
+++ Content.Server/Icarus/Commands/SpawnIcarusCommand.cs
@@ -0,0 +1,24 @@
+﻿using Content.Server.Administration;
+using Content.Shared.Administration;
+using JetBrains.Annotations;
+using Robust.Shared.Console;
+
+namespace Content.Server.Icarus.Commands;
+
+[UsedImplicitly]
+[AdminCommand(AdminFlags.Fun)]
+public sealed class SpawnIcarusCommand : IConsoleCommand
+{
+    public string Command => "spawnicarus";
+    public string Description => "Spawn Icarus beam.";
+    public string Help => "spawnicarus <gridId>";
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (args.Length < 1)
+        {
+            shell.WriteError("Incorrect number of arguments. " + Help);
+            return;
+        }
+    }
+}
