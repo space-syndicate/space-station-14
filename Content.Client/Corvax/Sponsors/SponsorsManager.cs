dbb7fe145cba69aee5964feba06a5d26d55f9878
Morbo <14136326+Morb0@users.noreply.github.com>
2022-11-15 04:00:48 +0300

    Send sponsor info to client

---
+++ Content.Client/Corvax/Sponsors/SponsorsManager.cs
@@ -0,0 +1,23 @@
+﻿using Content.Shared.Corvax.Sponsors;
+using Robust.Shared.Network;
+
+namespace Content.Client.Corvax.Sponsors;
+
+public sealed class SponsorsManager
+{
+    [Dependency] private readonly IClientNetManager _netMgr = default!;
+
+    public bool IsSponsor => Info != null;
+
+    public SponsorInfo? Info { get; private set; }
+
+    public void Initialize()
+    {
+        _netMgr.RegisterNetMessage<MsgSponsoringInfo>(HandleSponsoringInfo);
+    }
+
+    private void HandleSponsoringInfo(MsgSponsoringInfo message)
+    {
+        Info = message.Info;
+    }
+}
---
+++ Content.Shared/Corvax/Sponsors/MsgSponsorInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Lidgren.Network;
+using Robust.Shared.Network;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Corvax.Sponsors;
+
+/// <summary>
+/// Serve sends sponsoring info to client only if user is sponsor
+/// </summary>
+public sealed class MsgSponsoringInfo : NetMessage
+{
+    public override MsgGroups MsgGroup => MsgGroups.Command;
+    
+    public SponsorInfo Info = default!;
+    
+    public override void ReadFromBuffer(NetIncomingMessage buffer, IRobustSerializer serializer)
+    {
+        var length = buffer.ReadVariableInt32();
+        using var stream = buffer.ReadAlignedMemory(length);
+        serializer.DeserializeDirect(stream, out Info);
+    }
+
+    public override void WriteToBuffer(NetOutgoingMessage buffer, IRobustSerializer serializer)
+    {
+        using var stream = new MemoryStream();
+        serializer.SerializeDirect(stream, Info);
+        buffer.WriteVariableInt32((int) stream.Length);
+        stream.TryGetBuffer(out var segment);
+        buffer.Write(segment);
+    }
+}
