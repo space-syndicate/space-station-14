121d3e29a23bba70aceac839649bd57b7f833c40
Morbo <14136326+Morb0@users.noreply.github.com>
2022-12-23 14:26:57 +0300

    Better loadout items re-equipment logic

--- Content.Server/Corvax/Loadout/LoadoutSystem.cs
+++ Content.Server/Corvax/Loadout/LoadoutSystem.cs
@@ -2,6 +2,7 @@
 using Content.Server.Corvax.Sponsors;
 using Content.Server.GameTicking;
 using Content.Server.Hands.Systems;
+using Content.Server.Storage.EntitySystems;
 using Content.Shared.Inventory;
 using Robust.Shared.Prototypes;
 
@@ -10,9 +11,12 @@ namespace Content.Server.Corvax.Loadout;
 // NOTE: Full implementation will be in future, now just sponsor items
 public sealed class LoadoutSystem : EntitySystem
 {
+    private const string BackpackSlotId = "back";
+
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly InventorySystem _inventorySystem = default!;
     [Dependency] private readonly HandsSystem _handsSystem = default!;
+    [Dependency] private readonly StorageSystem _storageSystem = default!;
     [Dependency] private readonly SponsorsManager _sponsorsManager = default!;
     
     public override void Initialize()
@@ -26,6 +30,7 @@ public sealed class LoadoutSystem : EntitySystem
         {
             foreach (var loadoutId in sponsor.AllowedLoadout)
             {
+                // NOTE: Now is easy to not extract method because event give all info we need
                 if (_prototypeManager.TryIndex<LoadoutItemPrototype>(loadoutId, out var loadout))
                 {
                     var isSponsorOnly = loadout.SponsorOnly &&
@@ -43,17 +48,20 @@ public sealed class LoadoutSystem : EntitySystem
                         continue;
 
                     var entity = Spawn(loadout.EntityId, Transform(ev.Mob).Coordinates);
-                    if (loadout.SlotId != null)
-                    {
-                        if (_inventorySystem.TryGetSlotEntity(ev.Mob, loadout.SlotId, out var slotEntity))
-                            Del(slotEntity.Value); // Removing starting gear is okay, right...?
-
-                        _inventorySystem.TryEquip(ev.Mob, entity, loadout.SlotId, true);
-                    }
-                    else
+                    if (loadout.SlotId == null) // Take in hand if slot not specified
                     {
                         _handsSystem.TryPickup(ev.Mob, entity);
+                        continue;
                     }
+                    
+                    // Get occupied entity -> Insert to backpack -> Equip loadout gear
+                    if (_inventorySystem.TryGetSlotEntity(ev.Mob, loadout.SlotId, out var slotEntity) &&
+                        _inventorySystem.TryGetSlotEntity(ev.Mob, BackpackSlotId, out var backEntity) &&
+                        _storageSystem.CanInsert(backEntity.Value, slotEntity.Value, out _))
+                    {
+                        _storageSystem.Insert(backEntity.Value, slotEntity.Value);
+                    }
+                    _inventorySystem.TryEquip(ev.Mob, entity, loadout.SlotId, true);
                 }
             }
         }
