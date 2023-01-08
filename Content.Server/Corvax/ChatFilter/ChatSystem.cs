84bb6b46722c2f2b50cf998921091c2303d5a1c9
Morbo <14136326+Morb0@users.noreply.github.com>
2023-01-08 16:22:46 +0300

    Add chat word filter

---
+++ Content.Server/Corvax/ChatFilter/ChatSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Content.Server.Chat.Systems;
+
+public sealed partial class ChatSystem
+{
+    private static readonly Dictionary<string, string> SlangReplace = new()
+    {
+        { "кз", "корпоративный закон" },
+        { "дэк", "детектив" },
+        { "дек", "детектив" },
+        { "мш", "имплант защиты разума" },
+        { "лкм", "левая рука" },
+        { "пкм", "правая рука" },
+        { "трейтор", "предатель" },
+        { "инжи", "инженеры" },
+        { "инжинер", "инженеры" },
+        { "нюка", "ядерные оперативники" },
+        { "нюкеры", "ядерные оперативники" },
+        { "хз", "не знаю" },
+    };
+
+    private string ReplaceWords(string message)
+    {
+        if (string.IsNullOrEmpty(message))
+            return message;
+
+        foreach (var (word, replacement) in SlangReplace)
+            message = Regex.Replace(message, $"\\b{word}\\b", replacement);
+
+        return message;
+    }
+}
