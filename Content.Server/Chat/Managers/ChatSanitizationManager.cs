207e162507f2887084d4fc102b6fe32dff8daf0a
lapatison <100279397+lapatison@users.noreply.github.com>
2023-01-13 02:56:45 +0300

    chatsan2

--- Content.Server/Chat/Managers/ChatSanitizationManager.cs
+++ Content.Server/Chat/Managers/ChatSanitizationManager.cs
@@ -19,11 +19,8 @@ public sealed class ChatSanitizationManager : IChatSanitizationManager
         { "о/", "chatsan-waves" }, // cyrillic о
         { "о7", "chatsan-salutes" }, // cyrillic о
         { "0_o", "chatsan-wide-eyed" },
-        { "хех", "chatsan-laughs" },
-        { "хаха", "chatsan-laughs" },
-        { "ахах", "chatsan-laughs" },
-        { "ахаха", "chatsan-laughs" },
         { "лмао", "chatsan-laughs" },
+        { "рофл", "chatsan-laughs" },
         { "яхз", "chatsan-shrugs" },
         // Corvax-Localization-End
         // I could've done this with regex, but felt it wasn't the right idea.
