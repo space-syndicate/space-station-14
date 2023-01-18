ad2206a63e32d3c0d47d87533fa5a34831c4a7aa
Morbo <14136326+Morb0@users.noreply.github.com>
2023-01-19 01:10:01 +0300

    wip

---
+++ Content.Server/Corvax/PeacefulRoundEnd/PeacefulRoundEndSystem.cs
@@ -0,0 +1,17 @@
+﻿using Content.Shared.GameTicking;
+
+namespace Content.Server.Corvax.PeacefulRoundEnd;
+
+public sealed class PeacefulRoundEndSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<RoundEndedEvent>(OnRoundEnded);
+    }
+
+    private void OnRoundEnded(RoundEndedEvent ev)
+    {
+        var query = GetEntityQuery<>();
+    }
+}
