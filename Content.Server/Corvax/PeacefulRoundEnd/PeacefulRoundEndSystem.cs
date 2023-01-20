1b8778e401d36825610f8cb63e3cd8988f077350
Morbo <14136326+Morb0@users.noreply.github.com>
2023-01-20 13:56:43 +0300

    Add CCCVar and use Pacified

--- Content.Server/Corvax/PeacefulRoundEnd/PeacefulRoundEndSystem.cs
+++ Content.Server/Corvax/PeacefulRoundEnd/PeacefulRoundEndSystem.cs
@@ -1,17 +1,32 @@
-﻿using Content.Shared.GameTicking;
+﻿using Content.Server.GameTicking;
+using Content.Shared.CombatMode.Pacification;
+using Content.Shared.Corvax.CCCVars;
+using Robust.Server.Player;
+using Robust.Shared.Configuration;
 
 namespace Content.Server.Corvax.PeacefulRoundEnd;
 
 public sealed class PeacefulRoundEndSystem : EntitySystem
 {
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+    
+    private bool _isEnabled = false;
+    
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<RoundEndedEvent>(OnRoundEnded);
+        _cfg.OnValueChanged(CCCVars.PeacefulRoundEnd, v => _isEnabled = v, true);
+        SubscribeLocalEvent<RoundEndTextAppendEvent>(OnRoundEnded);
     }
 
-    private void OnRoundEnded(RoundEndedEvent ev)
+    private void OnRoundEnded(RoundEndTextAppendEvent ev)
     {
-        var query = GetEntityQuery<>();
+        if (!_isEnabled) return;
+        foreach (var session in _playerManager.Sessions)
+        {
+            if (!session.AttachedEntity.HasValue) continue;
+            EnsureComp<PacifiedComponent>(session.AttachedEntity.Value);
+        }
     }
 }
