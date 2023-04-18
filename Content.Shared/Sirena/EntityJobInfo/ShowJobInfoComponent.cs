450a71f369c018836b352c4dccad9fc88c4ea0a5
Anton Likhatskiy <Tinyismyson@gmail.com>
2023-04-18 18:03:00 +0500

    Part One. Primary mechanicals from Sirena for huds. Update in progress

---
+++ Content.Shared/Sirena/EntityJobInfo/ShowJobInfoComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared.Sirena.EntityJobInfo;
+
+/// <summary>
+/// This component allows you to see health bars above damageable mobs.
+/// </summary>
+[RegisterComponent]
+public sealed class ShowJobInfoComponent : Component
+{
+}
+
