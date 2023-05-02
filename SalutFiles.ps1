22cd149287ebc5f4297abcb3b3e82129b7721241
rg10008 <71792834+rg10008@users.noreply.github.com>
2023-05-03 02:45:39 +0400

    modified:   SalutFiles.ps1

--- SalutFiles.ps1
+++ SalutFiles.ps1
@@ -1,5 +1,9 @@
 New-Item -ItemType Junction -Path "Resources\Prototypes\SalutResourses" -Target "SalutResourses\Prototypes"
 New-Item -ItemType Junction -Path "Resources\Textures\ASalut" -Target "SalutResourses\Textures\ASalut"
 New-Item -ItemType Junction -Path "Resources\Locale\ru-RU\ASalut" -Target "SalutResourses\Locale\ru-RU\ASalut"
-Remove-Item -Path "C:\Users\rg10008\source\GameComplect\space-station-14\Resources\Locale\ru-RU\Job" -Recurse
+Remove-Item -Path "Resources\Locale\ru-RU\Job" -Recurse
+Remove-Item -Path "Resources\ConfigPresets" -Recurse
+Remove-Item -Path "Resources\Credits\Patrons.yml" -Recurse
+Remove-Item -Path "Resources\ServerInfo" -Recurse
 New-Item -ItemType Junction -Path "Resources\Locale\ru-RU\Job" -Target "SalutResourses\Locale\ru-RU\Job"
+New-Item -ItemType Junction -Path "Resources\ServerInfo" -Target "SalutResourses\ServerInfo"
