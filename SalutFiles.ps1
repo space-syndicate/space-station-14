175b93bd98437eb12dca83314a92ea7001c5c4f6
rg10008 <71792834+rg10008@users.noreply.github.com>
2023-05-03 01:42:31 +0400

    modified:   SalutFiles.ps1 	modified:   SalutResourses (modified content)

--- SalutFiles.ps1
+++ SalutFiles.ps1
@@ -1,1 +1,2 @@
 New-Item -ItemType Junction -Path "Resources\Prototypes\SalutResourses" -Target "SalutResourses\Prototypes"
+New-Item -ItemType Junction -Path "Resources\Textures\ASalut" -Target "SalutResourses\Textures\ASalut"
