ce00bd0c36121b45bc8cad686f7f54f7e8fd74a9
rg10008 <71792834+rg10008@users.noreply.github.com>
2023-05-03 13:51:57 +0400

    modified:   SalutFiles.ps1 	modified:   SalutResourses

--- SalutFiles.ps1
+++ SalutFiles.ps1
@@ -8,4 +8,4 @@ Remove-Item -Path "Resources\ServerInfo" -Recurse
 New-Item -ItemType Junction -Path "Resources\Locale\ru-RU\Job" -Target "SalutResourses\Locale\ru-RU\Job"
 New-Item -ItemType Junction -Path "Resources\ServerInfo" -Target "SalutResourses\ServerInfo"
 Remove-Item "bin\Content.Server\server_config.toml" -Recurse
-Move-Item "SalutResourses\ServerConfigs\server_config.toml" "bin\Content.Server\server_config.toml"
+Copy-Item "SalutResourses\ServerConfigs\server_config.toml" "bin\Content.Server\server_config.toml"
