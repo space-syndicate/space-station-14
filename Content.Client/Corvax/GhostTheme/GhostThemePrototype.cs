a8fe387b21ec2d6106594f07eb9586d929568772
Morb0 <14136326+Morb0@users.noreply.github.com>
2023-05-21 15:37:12 +0300

    Remove name field

--- Content.Client/Corvax/GhostTheme/GhostThemePrototype.cs
+++ Content.Client/Corvax/GhostTheme/GhostThemePrototype.cs
@@ -9,7 +9,5 @@ public sealed class GhostThemePrototype : IPrototype
     [IdDataField]
     public string ID { get; } = default!;
 
-    [DataField("name")] public string Name { get; set; } = "";
-
     [DataField("icon")] public string Icon { get; private set; } = string.Empty;
 }
