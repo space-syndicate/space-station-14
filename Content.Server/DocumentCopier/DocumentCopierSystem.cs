631d53b7d2d59e3440f23fa6198def9b95b68235
Smirnov <smirnopeteralex@yandex.ru>
2023-08-01 10:45:28 +0300

    Made copying logic

--- Content.Server/DocumentCopier/DocumentCopierSystem.cs
+++ Content.Server/DocumentCopier/DocumentCopierSystem.cs
@@ -1,12 +1,13 @@
-using Content.Server.DeviceNetwork;
 using Content.Server.DeviceNetwork.Systems;
 using Content.Server.Fax;
+using Content.Server.Paper;
+using Content.Server.Popups;
 using Content.Server.Power.Components;
 using Content.Server.UserInterface;
+using Content.Shared.Administration.Logs;
 using Content.Shared.Containers.ItemSlots;
+using Content.Shared.Database;
 using Content.Shared.DocumentCopier;
-using Content.Shared.Emag.Components;
-using Content.Shared.Fax;
 using Robust.Server.GameObjects;
 using Robust.Shared.Containers;
 
@@ -18,6 +19,10 @@ public sealed class DocumentCopierSystem : EntitySystem
     [Dependency] private readonly SharedAppearanceSystem _appearanceSystem = default!;
     [Dependency] private readonly UserInterfaceSystem _userInterface = default!;
     [Dependency] private readonly DeviceNetworkSystem _deviceNetworkSystem = default!;
+    [Dependency] private readonly ISharedAdminLogManager _adminLogger = default!;
+    [Dependency] private readonly PaperSystem _paperSystem = default!;
+    [Dependency] private readonly PopupSystem _popupSystem = default!;
+    [Dependency] private readonly EntityManager _entityManager = default!;
 
     private const string TargetPaperSlotId = "targetSheet";
     private const string SourcePaperSlotId = "sourceSheet";
@@ -34,7 +39,9 @@ public sealed class DocumentCopierSystem : EntitySystem
         SubscribeLocalEvent<DocumentCopierComponent, EntRemovedFromContainerMessage>(OnItemSlotChanged);
         SubscribeLocalEvent<DocumentCopierComponent, PowerChangedEvent>(OnPowerChanged);
 
+        // UI
         SubscribeLocalEvent<DocumentCopierComponent, AfterActivatableUIOpenEvent>(OnToggleInterface);
+        SubscribeLocalEvent<DocumentCopierComponent, DocumentCopierPrintMessage>(OnPrintButtonPressed);
     }
 
     private void OnMapInit(EntityUid uid, DocumentCopierComponent component, MapInitEvent args)
@@ -102,6 +109,61 @@ public sealed class DocumentCopierSystem : EntitySystem
     private void OnToggleInterface(EntityUid uid, DocumentCopierComponent component, AfterActivatableUIOpenEvent args)
     {
         UpdateAppearance(uid, component);
+    }
+
+    private void OnPrintButtonPressed(EntityUid uid, DocumentCopierComponent component, DocumentCopierPrintMessage args)
+    {
+        if (component.TargetSheet.Item.HasValue && TrySpawnPaper(uid, component))
+        {
+            EntityManager.DeleteEntity(component.TargetSheet.Item.Value);
+        }
+
+        UpdateUserInterface(uid, component);
+    }
+
+    private bool TrySpawnPaper(EntityUid uid, DocumentCopierComponent? component = null)
+    {
+        if (!Resolve(uid, ref component) || component.SourceSheet.Item == null || component.TargetSheet.Item == null)
+            return false;
+
+        var source = component.SourceSheet.Item;
+
+        if (!TryComp<PaperComponent>(component.TargetSheet.Item, out var targetPaper))
+            return false;
+        if (targetPaper.StampState != null)
+        {
+            _popupSystem.PopupEntity("Target paper was already stamped, it can't being override", uid);
+            return false;
+        }
+
+        const string entityToSpawn = "Paper";
+        var target = EntityManager.SpawnEntity(entityToSpawn, Transform(uid).Coordinates);
+
+        if (TryComp<PaperComponent>(source, out var sourcePaper))
+        {
+            if (sourcePaper.Content == "") // if source text is empty, it doesn't print
+            {
+                _popupSystem.PopupEntity("Source paper is empty, printing was automatically stopped", uid);
+                return true;
+            }
+
+            _paperSystem.SetContent(target, sourcePaper.Content);
+
+            // Apply stamps
+            if (sourcePaper.StampState != null)
+            {
+                foreach (var stampedBy in sourcePaper.StampedBy)
+                {
+                    _paperSystem.TryStamp(target, stampedBy, sourcePaper.StampState);
+                }
+            }
+        }
+
+        if (TryComp<MetaDataComponent>(target, out var targetMetadata) &&
+            TryComp<MetaDataComponent>(source, out var sourceMetadata))
+            targetMetadata.EntityName = sourceMetadata.EntityName;
+
+        return true;
     }
 
     // TODO: remake appearance to individual texture
