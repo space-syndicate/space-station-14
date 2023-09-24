13809298f7ee870d491c3514933f314f4a5a57ef
lzk <124214523+lzk228@users.noreply.github.com>
2023-09-24 20:06:52 +0200

    Update FrontalLispSystem.cs

--- Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
+++ Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
@@ -23,11 +23,11 @@ public sealed class FrontalLispSystem : EntitySystem
         message = Regex.Replace(message, @"[e]+[x]+[c]*|[x]+", "ekth");
         // Corvax-Localization Start
         // с, в, ч, т in ф or ш
-        message = Regex.Replace(message, @"[СВЧТ]", prob(50) ? "Ф":"Ш");
-        message = Regex.Replace(message, @"[СсВвЧчТт]", prob(50) ? "ф":"ш");
+        message = Regex.Replace(message, @"\B[СВЧТ]\B", _random.Prob(0.5f) ? "Ф" : "Ш");
+        message = Regex.Replace(message, @"\B[свчт]\B", _random.Prob(0.5f) ? "ф" : "ш");
         // д in ф
-        message = Regex.Replace(message, @"(?![ИЕЁЮЯЬ])[Д]", "Ф");
-        message = Regex.Replace(message, @"(?![ИиЕеЁёЮюЯяЬь])[Дд]", "ф");
+        message = Regex.Replace(message, @"\b[Д](?![ИЕЁЮЯЬ])\b|\B[Д]\B", "Ф");
+        message = Regex.Replace(message, @"\b[Дд](?![ИиЕеЁёЮюЯяЬь])\b|\B[Дд]\B", "ф");
         // Corvax-Localization End
         
         args.Message = message;
