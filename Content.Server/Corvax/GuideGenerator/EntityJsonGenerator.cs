68201fc1e806dad35d6f881ac61f8e9613fbd20d
mhamster <81412348+mhamsterr@users.noreply.github.com>
2023-09-24 21:29:13 +0700

    Fix wiki json autogen + entity autogen (#1441)

--- Content.Server/Corvax/GuideGenerator/EntityJsonGenerator.cs
+++ Content.Server/Corvax/GuideGenerator/EntityJsonGenerator.cs
@@ -1,35 +1,32 @@
 using System.IO;
 using System.Linq;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Content.Shared.Chemistry.Reaction;
 using Content.Shared.Chemistry.Reagent;
+using Content.Shared.Damage;
+using Content.Shared.FixedPoint;
 using Robust.Shared.Prototypes;
 
 namespace Content.Server.GuideGenerator;
 
-public sealed class ReactionJsonGenerator
+public sealed class EntityJsonGenerator
 {
     public static void PublishJson(StreamWriter file)
     {
         var prototype = IoCManager.Resolve<IPrototypeManager>();
-
-        var reactions =
+        var prototypes =
             prototype
-                .EnumeratePrototypes<ReactionPrototype>()
-                .Select(x => new ReactionEntry(x))
+                .EnumeratePrototypes<EntityPrototype>()
+                .Where(x => !x.Abstract)
+                .Select(x => new EntityEntry(x))
                 .ToDictionary(x => x.Id, x => x);
 
         var serializeOptions = new JsonSerializerOptions
         {
             WriteIndented = true,
-            NumberHandling = System.Text.Json.Serialization.JsonNumberHandling.AllowNamedFloatingPointLiterals, // Corvax-Wiki
-            Converters =
-            {
-                new UniversalJsonConverter<ReagentEffect>(),
-            }
         };
 
-        file.Write(JsonSerializer.Serialize(reactions, serializeOptions));
+        file.Write(JsonSerializer.Serialize(prototypes, serializeOptions));
     }
 }
-
