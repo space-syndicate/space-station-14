d049edd67b2d1e3c07ba0c56e0a690db28d4bac1
Morb0 <14136326+Morb0@users.noreply.github.com>
2023-10-03 02:46:20 +0300

    Add sponsor species

--- Content.Shared/Preferences/HumanoidCharacterProfile.cs
+++ Content.Shared/Preferences/HumanoidCharacterProfile.cs
@@ -380,6 +380,14 @@ namespace Content.Shared.Preferences
                 speciesPrototype = prototypeManager.Index<SpeciesPrototype>(Species);
             }
 
+            // Corvax-Sponsors-Start: Reset to human if player not sponsor
+            if (speciesPrototype.SponsorOnly && !sponsorPrototypes.Contains(Species))
+            {
+                Species = SharedHumanoidAppearanceSystem.DefaultSpecies;
+                speciesPrototype = prototypeManager.Index<SpeciesPrototype>(Species);
+            }
+            // Corvax-Sponsors-End
+
             var sex = Sex switch
             {
                 Sex.Male => Sex.Male,
