6e4608bf4fe3773868e5a041739add442240621a
Morb <14136326+Morb0@users.noreply.github.com>
2023-10-03 01:20:10 +0300

    [Secret] Add ghost themes (#1479)

--- Corvax/Content.Corvax.Interfaces.Client/IClientSponsorsManager.cs
+++ Corvax/Content.Corvax.Interfaces.Client/IClientSponsorsManager.cs
@@ -1,12 +1,8 @@
 ﻿using Content.Corvax.Interfaces.Shared;
-using Robust.Shared.Maths;
 
 namespace Content.Corvax.Interfaces.Client;
 
 public interface IClientSponsorsManager : ISharedSponsorsManager
 {
     public List<string> Prototypes { get; }
-    public bool PriorityJoin { get; }
-    public Color? OocColor { get; }
-    public int ExtraCharSlots { get; }
 }
