05f2b1fd81662abdf7a7de41005a83e8a04ef748
Morb0 <14136326+Morb0@users.noreply.github.com>
2023-10-03 14:40:27 +0300

    just directly call ln in publish workflow

--- Tools/corvax/setup_secrets.sh
+++
@@ -1,4 +0,0 @@
-﻿ln -s Secrets/Resources/Prototypes Resources/Prototypes/CorvaxSecrets
-ln -s Secrets/Resources/ServerPrototypes Resources/Prototypes/CorvaxSecretsServer
-ln -s Secrets/Resources/Locale Resources/Locale/ru-RU/corvax-secrets
-ln -s Secrets/Resources/Textures Resources/Textures/CorvaxSecrets
