c144a9ee75d5fab9a7d68666ef2a520c97328993
Morb0 <14136326+Morb0@users.noreply.github.com>
2023-10-03 14:34:04 +0300

    Fix setup_secrets.sh

--- Tools/corvax/setup_secrets.sh
+++ Tools/corvax/setup_secrets.sh
@@ -1,6 +1,4 @@
-﻿#!/bin/sh
-
-ln -s Secrets/Resources/Prototypes Resources/Prototypes/CorvaxSecrets
+﻿ln -s Secrets/Resources/Prototypes Resources/Prototypes/CorvaxSecrets
 ln -s Secrets/Resources/ServerPrototypes Resources/Prototypes/CorvaxSecretsServer
 ln -s Secrets/Resources/Locale Resources/Locale/ru-RU/corvax-secrets
 ln -s Secrets/Resources/Textures Resources/Textures/CorvaxSecrets
