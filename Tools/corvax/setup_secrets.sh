27e5c1d8f5522b12ed27e91aa6bd2c6cb5586f12
Morb0 <14136326+Morb0@users.noreply.github.com>
2023-10-03 14:24:25 +0300

    Add sh setup secrets script

---
+++ Tools/corvax/setup_secrets.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/sh
+
+ln -s Secrets/Resources/Prototypes Resources/Prototypes/CorvaxSecrets
+ln -s Secrets/Resources/ServerPrototypes Resources/Prototypes/CorvaxSecretsServer
+ln -s Secrets/Resources/Locale Resources/Locale/ru-RU/corvax-secrets
+ln -s Secrets/Resources/Textures Resources/Textures/CorvaxSecrets
