e6dfd39d944de0421d159c362ec2a13c78242e40
Ed <96445749+TheShuEd@users.noreply.github.com>
2023-12-17 19:35:39 +0300

    Update BarkAccentSystem.cs

--- Content.Server/Speech/EntitySystems/BarkAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/BarkAccentSystem.cs
@@ -8,7 +8,7 @@ namespace Content.Server.Speech.EntitySystems
         [Dependency] private readonly IRobustRandom _random = default!;
 
         private static readonly IReadOnlyList<string> Barks = new List<string>{
-            " Woof!", " WOOF", " wof-wof"
+            " Гав!", " ГАВ", " вуф-вуф" //Corvax localization
         }.AsReadOnly();
 
         private static readonly IReadOnlyDictionary<string, string> SpecialWords = new Dictionary<string, string>()
@@ -17,6 +17,11 @@ namespace Content.Server.Speech.EntitySystems
             { "Ah", "Arf" },
             { "oh", "oof" },
             { "Oh", "Oof" },
+            //Corvax localization
+            { "ага", "агаф" },
+            { "Ага", "Агаф" },
+            { "угу", "вуф" },
+            { "Угу", "Вуф" },
         };
 
         public override void Initialize()
@@ -32,7 +37,9 @@ namespace Content.Server.Speech.EntitySystems
             }
 
             return message.Replace("!", _random.Pick(Barks))
-                .Replace("l", "r").Replace("L", "R");
+                .Replace("l", "r").Replace("L", "R")
+                //Corvax localisation
+                .Replace("л", "р").Replace("Л", "Р");
         }
 
         private void OnAccent(EntityUid uid, BarkAccentComponent component, AccentGetEvent args)
