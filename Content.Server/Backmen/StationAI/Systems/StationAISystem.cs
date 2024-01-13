038e9613949ab19e78007f1100b03a21f4c0878b
Smirnov Peter <sh18rw@yandex.ru>
2024-01-13 17:13:11 +0300

    Some changes, AI doesn't work.

--- Content.Server/Backmen/StationAI/Systems/StationAISystem.cs
+++ Content.Server/Backmen/StationAI/Systems/StationAISystem.cs
@@ -1,14 +1,21 @@
+using Content.Server.Power.Components;
+using Content.Server.Storage.Components;
 using Content.Shared.Actions;
+using Content.Shared.Backmen.StationAI;
 using Robust.Shared.Prototypes;
 using Content.Shared.Backmen.StationAI.Events;
+using Content.Shared.Hands.Components;
+using Content.Shared.Hands.EntitySystems;
+using Content.Shared.Interaction.Events;
+using Content.Shared.Item;
 
-namespace Content.Shared.Backmen.StationAI;
+namespace Content.Server.Backmen.StationAI.Systems;
 
 public sealed class StationAISystem : EntitySystem
 {
-    [Dependency] private readonly IEntityManager _entityManager = default!;
     [Dependency] private readonly SharedActionsSystem _actions = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly SharedHandsSystem _hands = default!;
 
     public override void Initialize()
     {
@@ -16,13 +23,82 @@ public sealed class StationAISystem : EntitySystem
 
         SubscribeLocalEvent<StationAIComponent, ComponentStartup>(OnStartup);
         SubscribeLocalEvent<StationAIComponent, ComponentShutdown>(OnShutdown);
+        SubscribeLocalEvent<StationAIComponent, EntityTerminatingEvent>(OnTerminated);
+
+        SubscribeLocalEvent<StationAIComponent, InteractionAttemptEvent>(CanInteraction);
 
         SubscribeLocalEvent<AIHealthOverlayEvent>(OnHealthOverlayEvent);
+    }
+
+    private void CanInteraction(Entity<StationAIComponent> ent, ref InteractionAttemptEvent args)
+    {
+        var core = ent;
+        if (TryComp<AIEyeComponent>(ent, out var eye))
+        {
+            if (eye.AiCore == null)
+            {
+                QueueDel(ent);
+                args.Cancel();
+                return;
+            }
+            core = eye.AiCore.Value;
+        }
+        if (!core.Owner.Valid)
+        {
+            args.Cancel();
+            return;
+        }
+
+        if (args.Target != null && Transform(core).GridUid != Transform(args.Target.Value).GridUid)
+        {
+            args.Cancel();
+            return;
+        }
+
+        if (!TryComp<ApcPowerReceiverComponent>(core, out var power))
+        {
+            args.Cancel();
+            return;
+        }
+
+        if (power is { NeedsPower: true, Powered: false })
+        {
+            args.Cancel();
+            return;
+        }
+
+        if (HasComp<ItemComponent>(args.Target))
+        {
+            args.Cancel();
+            return;
+        }
+
+        if (HasComp<EntityStorageComponent>(args.Target))
+        {
+            args.Cancel();
+            return;
+        }
+
+        if (TryComp<ApcPowerReceiverComponent>(args.Target, out var targetPower) && targetPower.NeedsPower && !targetPower.Powered)
+        {
+            args.Cancel();
+            return;
+        }
+    }
+
+    private void OnTerminated(Entity<StationAIComponent> ent, ref EntityTerminatingEvent args)
+    {
+        if (!ent.Comp.ActiveEye.IsValid())
+        {
+            return;
+        }
+        QueueDel(ent.Comp.ActiveEye);
     }
 
     private void OnStartup(EntityUid uid, StationAIComponent component, ComponentStartup args)
     {
         _actions.AddAction(uid, ref component.ActionId, component.Action);
+        _hands.AddHand(uid,"SAI",HandLocation.Middle);
     }
 
     private void OnShutdown(EntityUid uid, StationAIComponent component, ComponentShutdown args)
@@ -32,7 +108,18 @@ public sealed class StationAISystem : EntitySystem
 
     private void OnHealthOverlayEvent(AIHealthOverlayEvent args)
     {
-        RaiseNetworkEvent(new NetworkedAIHealthOverlayEvent(GetNetEntity(args.Performer)));
-        args.Handled = true;
+        // if (HasComp<BkmShowHealthBarsComponent>(args.Performer))
+        // {
+        //     RemCompDeferred<BkmShowHealthBarsComponent>(args.Performer);
+        // }
+        // else
+        // {
+        //     var comp = EnsureComp<BkmShowHealthBarsComponent>(args.Performer);
+        //     comp.DamageContainers.Clear();
+        //     comp.DamageContainers.Add("Biological");
+        //     comp.DamageContainers.Add("HalfSpirit");
+        //     Dirty(args.Performer, comp);
+        // }
+        // args.Handled = true;
     }
 }
--- Content.Shared/Backmen/StationAI/Components/AIEyeComponent.cs
+++ Content.Shared/Backmen/StationAI/Components/AIEyeComponent.cs
@@ -1,12 +1,23 @@
 using Content.Shared.Actions;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Backmen.StationAI;
 
 [RegisterComponent]
 public sealed partial class AIEyeComponent : Component
 {
+    public Entity<StationAIComponent>? AiCore;
+
+    public EntProtoId ReturnAction = "AIEyeReturnAction";
+
+    public EntityUid? ReturnActionUid;
 }
 
 public sealed partial class AIEyePowerActionEvent : InstantActionEvent
+{
+
+}
+
+public sealed partial class AIEyePowerReturnActionEvent : InstantActionEvent
 {
 }
