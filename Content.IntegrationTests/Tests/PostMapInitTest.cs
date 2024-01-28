32b556d7dd77b611045e436b85d9af16a6c40fa8
Syxapik <127504249+Syxapik@users.noreply.github.com>
2024-01-28 17:42:30 +0700

    у

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -52,6 +52,7 @@ namespace Content.IntegrationTests.Tests
             "CorvaxOutpost",
             "CorvaxAstra",
             "CorvaxGelta",
+			"CorvaxMaus",
             // Corvax-End
             "Dev",
             "TestTeg",
