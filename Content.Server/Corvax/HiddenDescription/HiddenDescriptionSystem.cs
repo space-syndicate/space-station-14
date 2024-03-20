82d7017ec958c953a22d2a938f5675c302daaa4a
Morb <14136326+Morb0@users.noreply.github.com>
2024-03-21 00:12:35 +0300

    Update Content.Server/Corvax/HiddenDescription/HiddenDescriptionSystem.cs

--- Content.Server/Corvax/HiddenDescription/HiddenDescriptionSystem.cs
+++ Content.Server/Corvax/HiddenDescription/HiddenDescriptionSystem.cs
@@ -19,46 +19,15 @@ public sealed partial class HiddenDescriptionSystem : EntitySystem
 
     private void OnExamine(Entity<HiddenDescriptionComponent> hiddenDesc, ref ExaminedEvent args)
     {
-        _mind.TryGetMind(args.Examiner, out var mindId, out var mindComponent);
-        TryComp<JobComponent>(mindId, out var job);
+        bool isJobAllow = job?.Prototype != null && item.JobRequired.Contains(job.Prototype.Value);
+        bool isMindWhitelistPassed = item.WhitelistMind.IsValid(mindId);
+        bool isBodyWhitelistPassed = item.WhitelistMind.IsValid(args.Examiner);
 
-        foreach (var item in hiddenDesc.Comp.Entries)
-        {
-            //Check job
-            bool isJobAllow = false;
-            if (job != null && job.Prototype != null)
-            {
-                if (item.JobRequired.Contains(job.Prototype.Value))
-                {
-                    isJobAllow = true;
-                }
-            }
+         bool shouldPushMarkup = item.NeedAllCheck
+                ? isMindWhitelistPassed && isBodyWhitelistPassed && isJobAllow
+                : isMindWhitelistPassed || isBodyWhitelistPassed || isJobAllow;
 
-            //Check mind to whitelist
-            bool isMindWhitelistPassed = false;
-            if (item.WhitelistMind.IsValid(mindId))
-            {
-                isMindWhitelistPassed = true;
-            }
-
-            //Check body to whitelist
-            bool isBodyWhitelistPassed = false;
-            if (item.WhitelistMind.IsValid(args.Examiner))
-            {
-                isBodyWhitelistPassed = true;
-            }
-
-            //final check
-            if (item.NeedAllCheck)
-            {
-                if (isMindWhitelistPassed && isBodyWhitelistPassed && isJobAllow)
-                    args.PushMarkup(Loc.GetString(item.Label), hiddenDesc.Comp.PushPriority);
-            }
-            else
-            {
-                if (isMindWhitelistPassed || isBodyWhitelistPassed || isJobAllow)
-                    args.PushMarkup(Loc.GetString(item.Label), hiddenDesc.Comp.PushPriority);
-            }
-        }
+        if (shouldPushMarkup)
+                args.PushMarkup(Loc.GetString(item.Label), hiddenDesc.Comp.PushPriority);
     }
 }
