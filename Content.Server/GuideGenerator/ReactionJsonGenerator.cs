b059f677ba8b5630c9168e1b0d0035f109cb544b
p.krasnoshchekov <krasnoshchekov.pavel.spam@yandex.ru>
2024-04-25 06:00:04 +0300

    В выгрузку json на wiki рецептов и реагентов добавлены данные о: мин, макс температуре реакции, описание эффектов из гайдбука, название устройства для реакции (блендер, центрифуга..)

--- Content.Server/GuideGenerator/ReactionJsonGenerator.cs
+++ Content.Server/GuideGenerator/ReactionJsonGenerator.cs
@@ -9,6 +9,9 @@ namespace Content.Server.GuideGenerator;
 
 public sealed class ReactionJsonGenerator
 {
+    [ValidatePrototypeId<MixingCategoryPrototype>]
+    private const string DefaultMixingCategory = "DummyMix";
+
     public static void PublishJson(StreamWriter file)
     {
         var prototype = IoCManager.Resolve<IPrototypeManager>();
@@ -18,6 +21,29 @@ public sealed class ReactionJsonGenerator
                 .EnumeratePrototypes<ReactionPrototype>()
                 .Select(x => new ReactionEntry(x))
                 .ToDictionary(x => x.Id, x => x);
+
+        // MixingCategories
+        foreach (var reaction in reactions)
+        {
+            var reactionPrototype = prototype.Index<ReactionPrototype>(reaction.Key);
+            var mixingCategories = new List<MixingCategoryPrototype>();
+            if (reactionPrototype.MixingCategories != null)
+            {
+                foreach (var category in reactionPrototype.MixingCategories)
+                {
+                    mixingCategories.Add(prototype.Index(category));
+                }
+            }
+            else
+            {
+                mixingCategories.Add(prototype.Index<MixingCategoryPrototype>(DefaultMixingCategory));
+            }
+
+            foreach (var mixingCategory in mixingCategories)
+            {
+                reactions[reaction.Key].MixingCategories.Add(new MixingCategoryEntry(mixingCategory));
+            }
+        }
 
         var serializeOptions = new JsonSerializerOptions
         {
--- Content.Server/GuideGenerator/ReagentEntry.cs
+++ Content.Server/GuideGenerator/ReagentEntry.cs
@@ -28,11 +28,14 @@ public sealed class ReagentEntry
     [JsonPropertyName("color")]
     public string SubstanceColor { get; }
 
+    [JsonPropertyName("textColor")]
+    public string TextColor { get; }
+
     [JsonPropertyName("recipes")]
     public List<string> Recipes { get; } = new();
 
     [JsonPropertyName("metabolisms")]
-    public Dictionary<string, ReagentEffectsEntry>? Metabolisms { get; }
+    public Dictionary<string, List<ReagentEffectEntry>>? Metabolisms { get; }
 
     public ReagentEntry(ReagentPrototype proto)
     {
@@ -42,7 +45,15 @@ public sealed class ReagentEntry
         Description = proto.LocalizedDescription;
         PhysicalDescription = proto.LocalizedPhysicalDescription;
         SubstanceColor = proto.SubstanceColor.ToHex();
-        Metabolisms = proto.Metabolisms?.ToDictionary(x => x.Key.Id, x => x.Value);
+
+        var r = proto.SubstanceColor.R;
+        var g = proto.SubstanceColor.G;
+        var b = proto.SubstanceColor.B;
+        TextColor = (0.2126f * r + 0.7152f * g + 0.0722f * b > 0.5
+            ? Color.Black
+            : Color.White).ToHex();
+        
+        Metabolisms = proto.Metabolisms?.ToDictionary(x => x.Key.Id, x => x.Value.Effects.Select(x => new ReagentEffectEntry(x)).ToList());
     }
 }
 
@@ -60,11 +71,28 @@ public sealed class ReactionEntry
     [JsonPropertyName("products")]
     public Dictionary<string, float> Products { get; }
 
+    [JsonPropertyName("mixingCategories")]
+    public List<MixingCategoryEntry> MixingCategories { get; } = new();
+
+    [JsonPropertyName("minTemp")]
+    public float MinTemp { get; }
+
+    [JsonPropertyName("maxTemp")]
+    public float MaxTemp { get; }
+
+    [JsonPropertyName("hasMax")]
+    public bool HasMax { get; }
+
     [JsonPropertyName("effects")]
+    public List<ReagentEffectEntry> ExportEffects { get; }
+
+    [JsonIgnore]
     public List<ReagentEffect> Effects { get; }
 
     public ReactionEntry(ReactionPrototype proto)
     {
+        var protoMan = IoCManager.Resolve<IPrototypeManager>();
+
         Id = proto.ID;
         Name = TextTools.TextTools.CapitalizeString(proto.Name); // Corvax-Wiki
         Reactants =
@@ -75,7 +103,12 @@ public sealed class ReactionEntry
             proto.Products
                 .Select(x => KeyValuePair.Create(x.Key, x.Value.Float()))
                 .ToDictionary(x => x.Key, x => x.Value);
+
+        ExportEffects = proto.Effects.Select(x => new ReagentEffectEntry(x)).ToList();
         Effects = proto.Effects;
+        MinTemp = proto.MinimumTemperature;
+        MaxTemp = proto.MaximumTemperature;
+        HasMax = !float.IsPositiveInfinity(MaxTemp);
     }
 }
 
@@ -91,5 +124,38 @@ public sealed class ReactantEntry
     {
         Amount = amnt;
         Catalyst = cata;
+    }
+}
+
+public sealed class MixingCategoryEntry
+{
+    [JsonPropertyName("name")]
+    public string Name { get; }
+
+    [JsonPropertyName("id")]
+    public string Id { get; }
+
+    public MixingCategoryEntry(MixingCategoryPrototype proto)
+    {
+        Name = Loc.GetString(proto.VerbText);
+        Id = proto.ID;
+    }
+}
+
+public sealed class ReagentEffectEntry
+{
+    [JsonPropertyName("id")]
+    public string Id { get; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; }
+
+    public ReagentEffectEntry(ReagentEffect proto)
+    {
+        var prototype = IoCManager.Resolve<IPrototypeManager>();
+        var entSys = IoCManager.Resolve<IEntitySystemManager>();
+
+        Id = proto.GetType().Name;
+        Description = proto.GuidebookEffectDescription(prototype, entSys) ?? "";
     }
 }
