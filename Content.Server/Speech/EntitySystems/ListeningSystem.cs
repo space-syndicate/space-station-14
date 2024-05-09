515a3247f3a64ed60a73255773a3fa65039c7980
cl <v2v3r3@gmail.com>
2024-05-09 13:25:38 +0300

    testing

--- Content.Server/Speech/EntitySystems/ListeningSystem.cs
+++ Content.Server/Speech/EntitySystems/ListeningSystem.cs
@@ -18,10 +18,13 @@ public sealed class ListeningSystem : EntitySystem
 
     private void OnSpeak(EntitySpokeEvent ev)
     {
-        PingListeners(ev.Source, ev.Message, ev.ObfuscatedMessage);
+		if (HasComp<SecretMessageSourceComponent>(ev.Source))
+			PingListeners(ev.Source, ev.Message, ev.ObfuscatedMessage, true);
+		else
+			PingListeners(ev.Source, ev.Message, ev.ObfuscatedMessage, false);
     }
 
-    public void PingListeners(EntityUid source, string message, string? obfuscatedMessage)
+    public void PingListeners(EntityUid source, string message, string? obfuscatedMessage, bool secretMessage)
     {
         // TODO whispering / audio volume? Microphone sensitivity?
         // for now, whispering just arbitrarily reduces the listener's max range.
@@ -34,29 +37,56 @@ public sealed class ListeningSystem : EntitySystem
         var ev = new ListenEvent(message, source);
         var obfuscatedEv = obfuscatedMessage == null ? null : new ListenEvent(obfuscatedMessage, source);
         var query = EntityQueryEnumerator<ActiveListenerComponent, TransformComponent>();
+		
+		if (secretMessage){
+			while(query.MoveNext(out var listenerUid, out var listener, out var xform))
+			{
+				if (xform.MapID != sourceXform.MapID)
+					continue;
 
-        while(query.MoveNext(out var listenerUid, out var listener, out var xform))
-        {
-            if (xform.MapID != sourceXform.MapID)
-                continue;
+				// range checks
+				// TODO proper speech occlusion
+				var distance = (sourcePos - _xforms.GetWorldPosition(xform, xformQuery)).LengthSquared();
+				if ((distance > listener.Range * listener.Range) || HasComp<SecretMessageListenerComponent>(listenerUid))
+					continue;
 
-            // range checks
-            // TODO proper speech occlusion
-            var distance = (sourcePos - _xforms.GetWorldPosition(xform, xformQuery)).LengthSquared();
-            if (distance > listener.Range * listener.Range)
-                continue;
+				RaiseLocalEvent(listenerUid, attemptEv);
+				if (attemptEv.Cancelled)
+				{
+					attemptEv.Uncancel();
+					continue;
+				}
 
-            RaiseLocalEvent(listenerUid, attemptEv);
-            if (attemptEv.Cancelled)
-            {
-                attemptEv.Uncancel();
-                continue;
-            }
+				if (obfuscatedEv != null && distance > ChatSystem.WhisperClearRange)
+					RaiseLocalEvent(listenerUid, obfuscatedEv);
+				else
+					RaiseLocalEvent(listenerUid, ev);
+			}
+		}
+		else{
+			while(query.MoveNext(out var listenerUid, out var listener, out var xform))
+			{
+				if (xform.MapID != sourceXform.MapID)
+					continue;
 
-            if (obfuscatedEv != null && distance > ChatSystem.WhisperClearRange)
-                RaiseLocalEvent(listenerUid, obfuscatedEv);
-            else
-                RaiseLocalEvent(listenerUid, ev);
-        }
+				// range checks
+				// TODO proper speech occlusion
+				var distance = (sourcePos - _xforms.GetWorldPosition(xform, xformQuery)).LengthSquared();
+				if (distance > listener.Range * listener.Range)
+					continue;
+
+				RaiseLocalEvent(listenerUid, attemptEv);
+				if (attemptEv.Cancelled)
+				{
+					attemptEv.Uncancel();
+					continue;
+				}
+
+				if (obfuscatedEv != null && distance > ChatSystem.WhisperClearRange)
+					RaiseLocalEvent(listenerUid, obfuscatedEv);
+				else
+					RaiseLocalEvent(listenerUid, ev);
+			}
+		}
     }
 }
