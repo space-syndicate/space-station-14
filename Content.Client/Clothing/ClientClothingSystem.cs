418154e5ec687e3b95522c800031ab0c09a13aa5
hiucko <86206040+Hiucko@users.noreply.github.com>
2024-05-11 19:36:12 +0000

    Add socks

--- Content.Client/Clothing/ClientClothingSystem.cs
+++ Content.Client/Clothing/ClientClothingSystem.cs
@@ -44,6 +44,7 @@ public sealed class ClientClothingSystem : ClothingSystem
         {"pocket1", "POCKET1"},
         {"pocket2", "POCKET2"},
         {"suitstorage", "SUITSTORAGE"},
+        {"socks", "SOCKS"},
     };
 
     [Dependency] private readonly IResourceCache _cache = default!;
