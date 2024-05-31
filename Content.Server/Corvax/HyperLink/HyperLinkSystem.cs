2371b1840c946e3c25a0ab77959b8f50e166818d
JDtrimble <igor.mamaev1@gmail.com>
2024-05-31 14:26:02 +0300

    fixed item description and removed book read verb (it was rudimentary)

--- Content.Server/Corvax/HyperLink/HyperLinkSystem.cs
+++ Content.Server/Corvax/HyperLink/HyperLinkSystem.cs
@@ -2,7 +2,6 @@
 
 using Robust.Shared.Player;
 using Content.Shared.Interaction;
-using Content.Shared.Verbs;
 using Content.Shared.HyperLink;
 
 namespace Content.Server.HyperLink;
@@ -13,7 +12,6 @@ public sealed class HyperLinkSystem : EntitySystem
     {
         base.Initialize();
         SubscribeLocalEvent<HyperLinkComponent, ActivateInWorldEvent>(OnActivate);
-        SubscribeLocalEvent<HyperLinkComponent, GetVerbsEvent<AlternativeVerb>>(AddAltVerb);
     }
 
     private void OnActivate(EntityUid uid, HyperLinkComponent component, ActivateInWorldEvent args)
@@ -22,26 +20,6 @@ public sealed class HyperLinkSystem : EntitySystem
             return;
 
         OpenURL(actor.PlayerSession, component.URL);
-    }
-
-    private void AddAltVerb(EntityUid uid, HyperLinkComponent component, GetVerbsEvent<AlternativeVerb> args)
-    {
-        if (!args.CanAccess || !args.CanInteract)
-            return;
-
-        if (!TryComp<ActorComponent>(args.User, out var actor))
-            return;
-
-        AlternativeVerb verb = new()
-        {
-            Act = () =>
-            {
-                OpenURL(actor.PlayerSession, component.URL);
-            },
-            Text = Loc.GetString("book-read-verb"),
-            Priority = -2
-        };
-        args.Verbs.Add(verb);
     }
 
     public void OpenURL(ICommonSession session, string url)
