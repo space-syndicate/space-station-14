2861b0c2f4788b3c9b4df4e312d4ea80d6b517fc
XsenonDash <tiferi14@gmail.com>
2024-06-21 19:15:10 +0300

    PaperFix

--- Content.Server/Corvax/StationGoal/StationGoalPrototype.cs
+++ Content.Server/Corvax/StationGoal/StationGoalPrototype.cs
@@ -5,8 +5,17 @@ namespace Content.Server.Corvax.StationGoal
     [Serializable, Prototype("stationGoal")]
     public sealed class StationGoalPrototype : IPrototype
     {
-        [IdDataFieldAttribute] public string ID { get; } = default!;
+        [IdDataFieldAttribute]
+        public string ID { get; } = default!;
 
-        [DataField("text")] public string Text { get; set; } = string.Empty;
+        [DataField]
+        public string Text { get; set; } = string.Empty;
+
+        [DataField]
+        public int? MinPlayers;
+
+        [DataField]
+        public int? MaxPlayers;
+
     }
 }
--- Resources/Locale/ru-RU/corvax/preferences/loadout-groups.ftl
+++ Resources/Locale/ru-RU/corvax/preferences/loadout-groups.ftl
@@ -1,2 +1,2 @@
 loadout-group-inventory = Мой инвентарь
-loadout-group-psychologist-backpack = Рюкзак психолога
+loadout-group-chief-engineer-backpack = Рюкзак старшего инженера
