c99f33bd952de4272206edb957ea3a63e7052636
Evgencheg <73418250+Evgencheg@users.noreply.github.com>
2024-10-10 22:15:29 +0300

    Update translation.sh

--- Tools/ss14_ru/translation.sh
+++ Tools/ss14_ru/translation.sh
@@ -5,7 +5,7 @@ if [ "$(dirname $0)" != "." ]; then
     cd "$(dirname $0)"
 fi
 
-pip install -r requirements.txt
+pip install -r requirements.txt --no-warn-script-location
 python3 ./yamlextractor.py
 python3 ./keyfinder.py
 python3 ./clean_duplicates.py
