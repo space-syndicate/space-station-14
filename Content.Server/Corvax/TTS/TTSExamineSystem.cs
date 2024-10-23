835e079ec68d22754c44ff5c4c5c9c40020bdeed
lzk <124214523+lzk228@users.noreply.github.com>
2024-10-23 11:14:25 +0200

    Update TTSExamineSystem.cs

--- Content.Server/Corvax/TTS/TTSExamineSystem.cs
+++ Content.Server/Corvax/TTS/TTSExamineSystem.cs
@@ -8,60 +8,59 @@ using Robust.Shared.Configuration;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
-namespace Content.Server.Corvax.TTS
+namespace Content.Server.Corvax.TTS;
+
+public sealed class DetailExaminableSystem : EntitySystem
 {
-    public sealed class DetailExaminableSystem : EntitySystem
+    [Dependency] private readonly ExamineSystemShared _examineSystem = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] protected readonly IConfigurationManager _configManager = default!;
+
+    public override void Initialize()
     {
-        [Dependency] private readonly ExamineSystemShared _examineSystem = default!;
-        [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-        [Dependency] protected readonly IConfigurationManager _configManager = default!;
+        base.Initialize();
 
-        public override void Initialize()
+        SubscribeLocalEvent<TTSComponent, GetVerbsEvent<ExamineVerb>>(OnGetExamineVerbs);
+    }
+
+    private void OnGetExamineVerbs(EntityUid uid, TTSComponent component, GetVerbsEvent<ExamineVerb> args)
+    {
+        // Don't show verb if no TTS enabled
+        if (!_configManager.GetCVar(CCCVars.TTSEnabled))
+            return;
+
+        string? voiceId = string.Empty;
+
+        // If user is wearing a voice mask, we will take its voice
+        if (TryComp<VoiceMaskComponent>(uid, out var voiceMask))
+            voiceId = voiceMask.VoiceId;
+        else
+            voiceId = component.VoicePrototypeId;
+
+        // Get the voice name
+        string voiceName = string.Empty;
+        if (_prototypeManager.TryIndex<TTSVoicePrototype>(voiceId ?? string.Empty, out var protoVoice))
         {
-            base.Initialize();
-
-            SubscribeLocalEvent<TTSComponent, GetVerbsEvent<ExamineVerb>>(OnGetExamineVerbs);
+            voiceName = Loc.GetString(protoVoice.Name);
         }
 
-        private void OnGetExamineVerbs(EntityUid uid, TTSComponent component, GetVerbsEvent<ExamineVerb> args)
+        var detailsRange = _examineSystem.IsInDetailsRange(args.User, uid);
+
+        var verb = new ExamineVerb()
         {
-            // Don't show verb if no TTS enabled
-            if (!_configManager.GetCVar(CCCVars.TTSEnabled))
-                return;
-
-            string? voiceId = string.Empty;
-
-            // If user is wearing a voice mask, we will take its voice
-            if (TryComp<VoiceMaskComponent>(uid, out var voiceMask))
-                voiceId = voiceMask.VoiceId;
-            else
-                voiceId = component.VoicePrototypeId;
-
-            // Get the voice name
-            string voiceName = string.Empty;
-            if (_prototypeManager.TryIndex<TTSVoicePrototype>(voiceId ?? string.Empty, out var protoVoice))
+            Act = () =>
             {
-                voiceName = Loc.GetString(protoVoice.Name);
-            }
+                var markup = new FormattedMessage();
+                markup.AddMarkup(Loc.GetString("tts-examine-voice", ("name", voiceName)));
+                _examineSystem.SendExamineTooltip(args.User, uid, markup, false, false);
+            },
+            Text = Loc.GetString("tts-examine"),
+            Category = VerbCategory.Examine,
+            Disabled = !detailsRange,
+            Message = detailsRange ? null : Loc.GetString("tts-examine-disabled"),
+            Icon = new SpriteSpecifier.Texture(new ("/Textures/Interface/Emotes/vocal.png"))
+        };
 
-            var detailsRange = _examineSystem.IsInDetailsRange(args.User, uid);
-
-            var verb = new ExamineVerb()
-            {
-                Act = () =>
-                {
-                    var markup = new FormattedMessage();
-                    markup.AddMarkup(Loc.GetString("tts-examine-voice", ("name", voiceName)));
-                    _examineSystem.SendExamineTooltip(args.User, uid, markup, false, false);
-                },
-                Text = Loc.GetString("tts-examine"),
-                Category = VerbCategory.Examine,
-                Disabled = !detailsRange,
-                Message = detailsRange ? null : Loc.GetString("tts-examine-disabled"),
-                Icon = new SpriteSpecifier.Texture(new ("/Textures/Interface/Emotes/vocal.png"))
-            };
-
-            args.Verbs.Add(verb);
-        }
+        args.Verbs.Add(verb);
     }
 }
