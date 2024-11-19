f31197eb00cb674ba8d029c15d4f56db720a0ee0
Anri <elyasin.ane@mail.ru>
2024-11-19 18:04:34 +0300

    InitCommit

---
+++ Content.Server/Corvax/HiddenDescription/HiddenDescriptionContainerShowerComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Server.Corvax.HiddenDescription;
+
+/// <summary>
+/// Shows hidden description from entity in container
+/// </summary>
+[RegisterComponent]
+public sealed partial class HiddenDescriptionContainerShowerComponent : Component { }
