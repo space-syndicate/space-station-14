78143b1ce8c313d555b9259e5507672e10b1df6c
Morb0 <14136326+Morb0@users.noreply.github.com>
2024-12-26 00:42:19 +0300

    Apply typing state to holopad

--- Content.Server/Holopad/HolopadSystem.cs
+++ Content.Server/Holopad/HolopadSystem.cs
@@ -313,7 +313,7 @@ public sealed class HolopadSystem : SharedHolopadSystem
                 if (receiverHolopad.Comp.Hologram == null)
                     continue;
 
-                _appearanceSystem.SetData(receiverHolopad.Comp.Hologram.Value.Owner, TypingIndicatorVisuals.IsTyping, ev.IsTyping);
+                _appearanceSystem.SetData(receiverHolopad.Comp.Hologram.Value.Owner, TypingIndicatorVisuals.State, ev.TypingState);
             }
         }
     }
@@ -577,7 +577,7 @@ public sealed class HolopadSystem : SharedHolopadSystem
         {
             if (linkedHolopad.Comp.Hologram != null)
             {
-                _appearanceSystem.SetData(linkedHolopad.Comp.Hologram.Value.Owner, TypingIndicatorVisuals.IsTyping, false);
+                _appearanceSystem.SetData(linkedHolopad.Comp.Hologram.Value.Owner, TypingIndicatorVisuals.State, false);
 
                 // Send message with no sprite data to the client
                 // This will set the holgram sprite to a generic icon
--- Content.Shared/Holopad/HolopadUserComponent.cs
+++ Content.Shared/Holopad/HolopadUserComponent.cs
@@ -1,3 +1,4 @@
+using Content.Shared.Chat.TypingIndicator;
 using Robust.Shared.GameStates;
 using Robust.Shared.Serialization;
 
@@ -57,12 +58,12 @@ public sealed class HolopadUserTypingChangedEvent : EntityEventArgs
     /// <summary>
     /// The typing indicator state
     /// </summary>
-    public readonly bool IsTyping;
+    public readonly TypingIndicatorState TypingState;
 
-    public HolopadUserTypingChangedEvent(NetEntity user, bool isTyping)
+    public HolopadUserTypingChangedEvent(NetEntity user, TypingIndicatorState state)
     {
         User = user;
-        IsTyping = isTyping;
+        TypingState = state;
     }
 }
 
