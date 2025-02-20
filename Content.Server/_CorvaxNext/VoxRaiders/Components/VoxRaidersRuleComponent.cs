538c869bf0ca61f5ba4ed4c2767d77f7bddb2789
FN <firenamefn@gmail.com>
2025-02-20 19:24:23 +0700

    Objectives

--- Content.Server/_CorvaxNext/VoxRaiders/Components/VoxRaidersRuleComponent.cs
+++ Content.Server/_CorvaxNext/VoxRaiders/Components/VoxRaidersRuleComponent.cs
@@ -1,4 +1,7 @@
 namespace Content.Server._CorvaxNext.VoxRaiders.Components;
 
 [RegisterComponent]
-public sealed partial class VoxRaidersRuleComponent : Component;
+public sealed partial class VoxRaidersRuleComponent : Component
+{
+    public ExtractionShuttleComponent? Shuttle;
+}
--- Content.Server/_CorvaxNext/VoxRaiders/EntitySystems/VoxRaidersRuleSystem.cs
+++ Content.Server/_CorvaxNext/VoxRaiders/EntitySystems/VoxRaidersRuleSystem.cs
@@ -1,24 +1,52 @@
 using Content.Server._CorvaxNext.VoxRaiders.Components;
 using Content.Server.Antag;
+using Content.Server.GameTicking;
 using Content.Server.GameTicking.Rules;
-using Content.Server.GameTicking.Rules.Components;
-using Content.Shared.Mind;
+using Content.Shared.GameTicking.Components;
+using Content.Shared.Players;
+using Robust.Shared.Map.Components;
 
 namespace Content.Server._CorvaxNext.VoxRaiders.EntitySystems;
 
 public sealed class VoxRaidersRuleSystem : GameRuleSystem<VoxRaidersRuleComponent>
 {
-    [Dependency] private readonly SharedMindSystem _mind = default!;
-
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<VoxRaidersRuleComponent, AfterAntagEntitySelectedEvent>(OnAfterAntagEntitySelected);
+        SubscribeLocalEvent<VoxRaidersRuleComponent, RuleLoadedGridsEvent>(OnRuleLoadedGrids);
+    }
+
+    protected override void AppendRoundEndText(EntityUid uid, VoxRaidersRuleComponent component, GameRuleComponent gameRule, ref RoundEndTextAppendEvent args)
+    {
+        
     }
 
     private void OnAfterAntagEntitySelected(Entity<VoxRaidersRuleComponent> entity, ref AfterAntagEntitySelectedEvent e)
     {
-        //_mind.TryAddObjective();
+        var mind = e.Session?.GetMind();
+
+        if (mind is null)
+            return;
+
+        entity.Comp.Shuttle?.Owners.Add(mind.Value);
+    }
+
+    private void OnRuleLoadedGrids(Entity<VoxRaidersRuleComponent> entity, ref RuleLoadedGridsEvent e)
+    {
+        var query1 = AllEntityQuery<MapGridComponent>();
+        while (query1.MoveNext(out var ent, out var shuttle))
+        {
+            EnsureComp<ExtractionShuttleComponent>(ent);
+        }
+
+        var query = AllEntityQuery<ExtractionShuttleComponent>();
+        while (query.MoveNext(out _, out var shuttle))
+        {
+            entity.Comp.Shuttle = shuttle;
+
+            return;
+        }
     }
 }
