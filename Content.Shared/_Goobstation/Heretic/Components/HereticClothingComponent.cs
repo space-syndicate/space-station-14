593168995bd0b0d02af17f9a6dd5e26833e9e3dc
ksen0morph <144564556+ksen0morph@users.noreply.github.com>
2025-03-24 11:51:14 +0300

    linter again

---
+++ Content.Shared/_Goobstation/Heretic/Components/HereticClothingComponent.cs
@@ -0,0 +1,9 @@
+namespace Content.Shared._Goobstation.Heretic.Components;
+
+/// <summary>
+/// Prevents non heretic to wear clothing with this component
+/// </summary>
+[RegisterComponent]
+public sealed partial class HereticClothingComponent : Component
+{
+}
