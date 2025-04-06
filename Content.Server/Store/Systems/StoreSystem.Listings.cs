c43004f1672be766734d16757a1a800c21f5e318
ksen0morph <144564556+ksen0morph@users.noreply.github.com>
2025-04-06 03:03:04 +0300

    heretic monkey hotfix (#339)
    * heretic fix (aka revert #35334)
    
    * some fixes
    
    * oops

--- Content.Server/Store/Systems/StoreSystem.Listings.cs
+++ Content.Server/Store/Systems/StoreSystem.Listings.cs
@@ -118,7 +118,7 @@ public sealed partial class StoreSystem
 
             if (listing.Conditions != null)
             {
-                var args = new ListingConditionArgs(GetBuyerMind(buyer), storeEntity, listing, EntityManager);
+                var args = new ListingConditionArgs(buyer, storeEntity, listing, EntityManager);
                 var conditionsMet = true;
 
                 foreach (var condition in listing.Conditions)
@@ -143,13 +143,6 @@ public sealed partial class StoreSystem
     /// If it doesn't have one, or is a mind entity already, it returns itself.
     /// </summary>
     /// <param name="buyer">The buying entity.</param>
-    public EntityUid GetBuyerMind(EntityUid buyer)
-    {
-        if (!HasComp<MindComponent>(buyer) && _mind.TryGetMind(buyer, out var buyerMind, out var _))
-            return buyerMind;
-
-        return buyer;
-    }
 
     /// <summary>
     /// Checks if a listing appears in a list of given categories
