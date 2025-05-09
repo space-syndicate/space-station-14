e5308b2871d9bd55c6c581ca6d4da8114eac621a
Krosus777 <38509947+Krosus777@users.noreply.github.com>
2025-05-09 19:40:58 +0200

    Part3

--- Content.Server/Corvax/Ipc/IpcSystem.cs
+++ Content.Server/Corvax/Ipc/IpcSystem.cs
@@ -73,7 +73,7 @@ public sealed class IpcSystem : EntitySystem
     {
 
 
-        if (!_powerCell.TryGetBatteryFromSlot(ent, out var battery, slot) || MathHelper.CloseToPercent(battery.CurrentCharge, 0))
+        if (!_powerCell.TryGetBatteryFromSlot(ent, out var battery, slot) || battery.CurrentCharge < 0.01f)
         {
             _alerts.ClearAlert(ent, ent.Comp.BatteryAlert);
             _alerts.ShowAlert(ent, ent.Comp.NoBatteryAlert);
