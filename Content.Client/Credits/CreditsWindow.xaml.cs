cdaf01246cb2d21adb363ba579ea16fcb794dae5
Dmitry <57028746+dimm00n@users.noreply.github.com>
2025-05-21 03:08:51 +0700

    the first fix

--- Content.Client/Credits/CreditsWindow.xaml.cs
+++ Content.Client/Credits/CreditsWindow.xaml.cs
@@ -353,6 +353,7 @@ public sealed partial class CreditsWindow : DefaultWindow
             ss14ContributorsContainer.AddChild(label);
         }
 
+        AddSection(Loc.GetString("credits-window-tts-title"), "TTS.txt"); // Corvax-TTS
         AddSection(Loc.GetString("credits-window-contributors-section-title"), "GitHub.txt");
         AddSection(Loc.GetString("credits-window-codebases-section-title"), "SpaceStation13.txt");
         AddSection(Loc.GetString("credits-window-original-remake-team-section-title"), "OriginalRemake.txt");
