de982e2f4c233774f08c80f746584fdb86d3801f
Dmitry <57028746+DIMMoon1@users.noreply.github.com>
2025-08-26 03:47:26 +0700

    Fix sponsor marking load order (#3369)

--- Content.Server/Preferences/Managers/ServerPreferencesManager.cs
+++ Content.Server/Preferences/Managers/ServerPreferencesManager.cs
@@ -255,16 +255,6 @@ namespace Content.Server.Preferences.Managers
                 async Task LoadPrefs()
                 {
                     var prefs = await GetOrCreatePreferencesAsync(session.UserId, cancel);
-                    // Corvax-Sponsors-Start: Remove sponsor markings from expired sponsors
-                    var collection = IoCManager.Instance!;
-                    foreach (var (_, profile) in prefs.Characters)
-                    {
-                        var sponsorPrototypes = _sponsors != null && _sponsors.TryGetServerPrototypes(session.UserId, out var prototypes)
-                            ? prototypes.ToArray()
-                            : [];
-                        profile.EnsureValid(session, collection, sponsorPrototypes);
-                    }
-                    // Corvax-Sponsors-End
                     prefsData.Prefs = prefs;
                 }
             }
@@ -280,6 +270,17 @@ namespace Content.Server.Preferences.Managers
             prefsData.Prefs = SanitizePreferences(session, prefsData.Prefs, _dependencies);
 
             prefsData.PrefsLoaded = true;
+
+            // Corvax-Sponsors-Start: Remove sponsor markings from expired sponsors
+            var collection = IoCManager.Instance!;
+            foreach (var (_, profile) in prefsData.Prefs.Characters)
+            {
+                var sponsorPrototypes = _sponsors != null && _sponsors.TryGetServerPrototypes(session.UserId, out var prototypes)
+                    ? prototypes.ToArray()
+                    : [];
+                profile.EnsureValid(session, collection, sponsorPrototypes);
+            }
+            // Corvax-Sponsors-End
 
             var msg = new MsgPreferencesAndSettings();
             msg.Preferences = prefsData.Prefs;
