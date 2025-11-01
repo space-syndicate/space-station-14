58762cc4ff66ee7e008fe56d6f4122cb2caa0176
kosticia <kosticia46@gmail.com>
2025-11-01 20:08:15 +0300

    аааааааааааааа

--- Content.Server/Speech/EntitySystems/FrenchAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/FrenchAccentSystem.cs
@@ -1,6 +1,7 @@
 using System.Text.RegularExpressions;
 using Content.Server.Speech.Components;
 using Content.Shared.Speech;
+using Robust.Shared.Random;
 
 namespace Content.Server.Speech.EntitySystems;
 
@@ -10,9 +11,15 @@ namespace Content.Server.Speech.EntitySystems;
 public sealed class FrenchAccentSystem : EntitySystem
 {
     [Dependency] private readonly ReplacementAccentSystem _replacement = default!;
+    // Russian-Locale-Start
+    [Dependency] private readonly IRobustRandom _random = default!;
+    // Russian-Locale-End
 
     private static readonly Regex RegexTh = new(@"th", RegexOptions.IgnoreCase);
-    private static readonly Regex RegexStartH = new(@"(?<!\w)h", RegexOptions.IgnoreCase);
+    private static readonly Regex RegexH = new("х", RegexOptions.IgnoreCase);
+    private static readonly Regex RegexR = new("[рР]+", RegexOptions.IgnoreCase);
+    private static readonly Regex RegexE = new("e", RegexOptions.IgnoreCase);
+    private static readonly Regex RegexZ = new("з", RegexOptions.IgnoreCase);
     private static readonly Regex RegexSpacePunctuation = new(@"(?<=\w\w)[!?;:](?!\w)", RegexOptions.IgnoreCase);
 
     public override void Initialize()
@@ -27,9 +34,6 @@ public sealed class FrenchAccentSystem : EntitySystem
         var msg = message;
 
         msg = _replacement.ApplyReplacements(msg, "french");
-
-        // replaces h with ' at the start of words.
-        msg = RegexStartH.Replace(msg, "'");
 
         // spaces out ! ? : and ;.
         msg = RegexSpacePunctuation.Replace(msg, " $&");
@@ -51,7 +55,70 @@ public sealed class FrenchAccentSystem : EntitySystem
             }
         }
 
+        // Russian-Locale
+
+        foreach (Match match in RegexH.Matches(msg))
+        {
+            var uppercase = msg[match.Index] == 'Х';
+            var g = uppercase ? "Г" : "г";
+
+            // Если х стоит первой, то с некоторым шансом превращаем её в апостроф
+            if (match.Index == 0 && _random.Prob(0.5f) || match.Index != 0 && char.IsWhiteSpace(msg[match.Index - 1]))
+            {
+                msg = "'" + msg[(match.Index + 1)..];
+            }
+            // В противном случае, тупо конвентируем её в г
+            else
+            {
+                msg = msg[..match.Index] + g + msg[(match.Index + 1)..];
+            }
+        }
+
+        foreach (Match match in RegexR.Matches(msg))
+        {
+            var uppercase = msg[match.Index] == 'Р';
+            var g = uppercase ? "Г" : "г";
+            var h = "";
+            for (var i = 0; i < match.Value.Length; i++)
+            {
+                // Блять я не буду это комментировать.
+                if (uppercase && match.Index != 0 && !char.IsWhiteSpace(msg[match.Index - 1]) || msg[match.Index + match.Length].ToString().Equals(msg[match.Index + match.Length].ToString().ToUpper(), StringComparison.CurrentCulture))
+                    h += "Х";
+                else
+                    h += "х";
+            }
+
+            // Превращаем р в гх
+            msg = msg[..match.Index] + g + h + msg[(match.Index + h.Length)..];
+        }
+
+        foreach (Match match in RegexE.Matches(msg))
+        {
+            var uppercase = msg[match.Index] == 'Е';
+            var e = uppercase ? "Э" : "э";
+
+            // Превращаем е в э перед другими буквами
+            if (match.Index != 0 && !char.IsWhiteSpace(msg[match.Index - 1]))
+            {
+                msg = msg[..match.Index] + e + msg[(match.Index + 1)..];
+            }
+        }
+
+        foreach (Match match in RegexZ.Matches(msg))
+        {
+            var uppercase = msg[match.Index] == 'З';
+            var z = uppercase ? "Ж" : "ж";
+
+            // С шансом в 10 процентов превращаем з в ж для изображения звонкости
+            // ТУДУ: хз, мб ещё тильду после з влепливать с некоторым шансом?
+            if (_random.Prob(0.1f))
+            {
+                msg = msg[..match.Index] + z + msg[(match.Index + 1)..];
+            }
+        }
+
         return msg;
+
     }
 
     private void OnAccentGet(EntityUid uid, FrenchAccentComponent component, AccentGetEvent args)
