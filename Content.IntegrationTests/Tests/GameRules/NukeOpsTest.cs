7a52718ccd2dd36315ed8906a2c491ce5bc92198
JrInventor05Next <dorokhovartem2010@gmail.com>
2025-12-18 15:51:40 +0800

    NukeOpsTest

--- Content.IntegrationTests/Tests/GameRules/NukeOpsTest.cs
+++ Content.IntegrationTests/Tests/GameRules/NukeOpsTest.cs
@@ -229,12 +229,16 @@ public sealed class NukeOpsTest
         var totalSeconds = 30;
         var totalTicks = (int) Math.Ceiling(totalSeconds / server.Timing.TickPeriod.TotalSeconds);
         var increment = 5;
-        var resp = entMan.GetComponent<RespiratorComponent>(player);
+        var isIPC = entMan.GetComponent<MetaDataComponent>(player).EntityPrototype.ID == "MobIpc" ? true : false;
+        RespiratorComponent? resp = null;
+        if (!isIPC)
+            resp = entMan.GetComponent<RespiratorComponent>(player);
         var damage = entMan.GetComponent<DamageableComponent>(player);
         for (var tick = 0; tick < totalTicks; tick += increment)
         {
             await pair.RunTicksSync(increment);
-            Assert.That(resp.SuffocationCycles, Is.LessThanOrEqualTo(resp.SuffocationCycleThreshold));
+            if (!isIPC)
+                Assert.That(resp.SuffocationCycles, Is.LessThanOrEqualTo(resp.SuffocationCycleThreshold));
             Assert.That(damage.TotalDamage, Is.EqualTo(FixedPoint2.Zero));
         }
 
