49794858f2b5b5ec0aa5bb900e7a52a3d84cff97
Jerry <48524572+JerryImMouse@users.noreply.github.com>
2026-02-07 22:06:45 +0500

    Fix Content.Packaging doesn't include secrets into a final server package (#3502)

--- Content.Packaging/ServerPackaging.cs
+++ Content.Packaging/ServerPackaging.cs
@@ -235,15 +235,13 @@ public static class ServerPackaging
         var depsContentExclusive = depsContent.Except(depsRobust).ToHashSet();
 
         // Remove .dll suffix and apply filtering.
-        var names = depsContentExclusive.Select(p => p[..^4]).Where(p => !ServerNotExtraAssemblies.Any(p.StartsWith));
+        var names = depsContentExclusive.Select(p => p[..^4]).Where(p => !ServerNotExtraAssemblies.Any(p.StartsWith)).ToList();
 
         // Corvax-Secrets-Start
-        names.Append("Content.Corvax.Interfaces.Shared");
-        names.Append("Content.Corvax.Interfaces.Server");
         if (UseSecrets)
         {
-            names.Append("Content.Corvax.Shared");
-            names.Append("Content.Corvax.Server");
+            names.Add("Content.Corvax.Shared");
+            names.Add("Content.Corvax.Server");
         }
         // Corvax-Secrets-End
 
