9b06d3f7614d3c70e6835518e8d2e91c289c42dd
Pok <113675512+Pok27@users.noreply.github.com>
2026-03-09 17:42:57 +0200

    [Wiki] fix update-wiki (part 99) (#3525)

--- Content.Server/Corvax/GuideGenerator/FieldStoreId.cs
+++ Content.Server/Corvax/GuideGenerator/FieldStoreId.cs
@@ -1,7 +1,7 @@
 using System.Collections;
+using System.Linq;
 using System.Reflection;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization.Manager;
 using Robust.Shared.Serialization.Markdown;
 using Robust.Shared.Serialization.Markdown.Mapping;
 using Robust.Shared.Serialization.Markdown.Sequence;
@@ -11,6 +11,11 @@ namespace Content.Server.Corvax.GuideGenerator;
 
 public static class FieldStoreId
 {
+    private const string TypeTagPrefix = "!type:";
+    private static readonly Dictionary<(Type DeclaredType, string Tag), Type> ConcreteTypeCache = new();
+    private static readonly Dictionary<Type, Type[]> ConcreteAssignableTypesCache = new();
+    private static readonly Dictionary<Type, HashSet<string>> DataFieldTagsCache = new();
+
     public static void InspectTypeForEntityRefs(Type prototypeType, MappingDataNode mapping, HashSet<string> outIds)
     {
         foreach (var field in prototypeType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
@@ -42,7 +47,7 @@ public static class FieldStoreId
 
     public static void ExtractIdsFromNode(Type memberType, DataNode node, HashSet<string> outIds)
     {
-        var underlying = GetEffectiveUnderlyingType(memberType, node);
+        var underlying = ResolveConcreteType(Nullable.GetUnderlyingType(memberType) ?? memberType, node);
 
         if (IsEntProtoIdType(underlying))
         {
@@ -118,39 +123,6 @@ public static class FieldStoreId
         }
     }
 
-    private static Type GetEffectiveUnderlyingType(Type memberType, DataNode node)
-    {
-        var underlying = Nullable.GetUnderlyingType(memberType) ?? memberType;
-
-        if (node is not MappingDataNode map)
-            return underlying;
-
-        var tag = map.Tag;
-        if (string.IsNullOrEmpty(tag))
-            return underlying;
-
-        const string prefix = "!type:";
-        if (!tag.StartsWith(prefix, StringComparison.Ordinal))
-            return underlying;
-
-        var typeName = tag.Substring(prefix.Length);
-
-        try
-        {
-            var ser = IoCManager.Resolve<ISerializationManager>();
-            var reflection = ser.ReflectionManager;
-            var actual = reflection.YamlTypeTagLookup(underlying, typeName);
-            if (actual != null && underlying.IsAssignableFrom(actual))
-                return actual;
-        }
-        catch
-        {
-            // ignored
-        }
-
-        return underlying;
-    }
-
     private static bool IsEntProtoIdType(Type t)
     {
         if (t == typeof(EntProtoId))
@@ -160,6 +132,165 @@ public static class FieldStoreId
             return true;
 
         return false;
+    }
+
+    private static Type ResolveConcreteType(Type declaredType, DataNode node)
+    {
+        if (node is MappingDataNode map)
+        {
+            var inferred = InferConcreteType(declaredType, map);
+            if (inferred != declaredType)
+                return inferred;
+        }
+
+        if (node.Tag == null || !node.Tag.StartsWith(TypeTagPrefix, StringComparison.Ordinal))
+            return declaredType;
+
+        var typeName = node.Tag[TypeTagPrefix.Length..];
+        if (string.IsNullOrWhiteSpace(typeName))
+            return declaredType;
+
+        var cacheKey = (declaredType, typeName);
+        if (ConcreteTypeCache.TryGetValue(cacheKey, out var cachedType))
+            return cachedType;
+
+        foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
+        {
+            Type[] types;
+            try
+            {
+                types = asm.GetTypes();
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                types = e.Types.Where(t => t != null).Cast<Type>().ToArray();
+            }
+
+            foreach (var type in types)
+            {
+                if (type.IsAbstract || type.IsInterface)
+                    continue;
+
+                if (!declaredType.IsAssignableFrom(type))
+                    continue;
+
+                if (!string.Equals(type.Name, typeName, StringComparison.Ordinal))
+                    continue;
+
+                ConcreteTypeCache[cacheKey] = type;
+                return type;
+            }
+        }
+
+        ConcreteTypeCache[cacheKey] = declaredType;
+        return declaredType;
+    }
+
+    private static Type InferConcreteType(Type declaredType, MappingDataNode node)
+    {
+        var nodeKeys = node.Keys.ToHashSet(StringComparer.Ordinal);
+        if (nodeKeys.Count == 0)
+            return declaredType;
+
+        var candidates = GetConcreteAssignableTypes(declaredType);
+        if (candidates.Length == 0)
+            return declaredType;
+
+        Type? bestType = null;
+        var bestScore = -1;
+        var ambiguous = false;
+
+        foreach (var candidate in candidates)
+        {
+            var candidateTags = GetDataFieldTags(candidate);
+            if (candidateTags.Count == 0)
+                continue;
+
+            if (!nodeKeys.IsSubsetOf(candidateTags))
+                continue;
+
+            var score = nodeKeys.Count;
+
+            if (score > bestScore)
+            {
+                bestType = candidate;
+                bestScore = score;
+                ambiguous = false;
+                continue;
+            }
+
+            if (score == bestScore)
+                ambiguous = true;
+        }
+
+        if (ambiguous || bestType == null)
+            return declaredType;
+
+        return bestType;
+    }
+
+    private static Type[] GetConcreteAssignableTypes(Type declaredType)
+    {
+        if (ConcreteAssignableTypesCache.TryGetValue(declaredType, out var cached))
+            return cached;
+
+        var types = new List<Type>();
+        foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
+        {
+            Type[] assemblyTypes;
+            try
+            {
+                assemblyTypes = asm.GetTypes();
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                assemblyTypes = e.Types.Where(t => t != null).Cast<Type>().ToArray();
+            }
+
+            foreach (var type in assemblyTypes)
+            {
+                if (type.IsAbstract || type.IsInterface)
+                    continue;
+
+                if (!declaredType.IsAssignableFrom(type))
+                    continue;
+
+                types.Add(type);
+            }
+        }
+
+        cached = types.ToArray();
+        ConcreteAssignableTypesCache[declaredType] = cached;
+        return cached;
+    }
+
+    private static HashSet<string> GetDataFieldTags(Type type)
+    {
+        if (DataFieldTagsCache.TryGetValue(type, out var cached))
+            return cached;
+
+        var tags = new HashSet<string>(StringComparer.Ordinal);
+
+        foreach (var field in type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+        {
+            var attr = field.GetCustomAttribute<DataFieldAttribute>();
+            if (attr == null)
+                continue;
+
+            tags.Add(attr.Tag ?? LowerFirst(field.Name));
+        }
+
+        foreach (var prop in type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+        {
+            var attr = prop.GetCustomAttribute<DataFieldAttribute>();
+            if (attr == null)
+                continue;
+
+            tags.Add(attr.Tag ?? LowerFirst(prop.Name));
+        }
+
+        DataFieldTagsCache[type] = tags;
+        return tags;
     }
 
     private static Type? GetElementType(Type t)
