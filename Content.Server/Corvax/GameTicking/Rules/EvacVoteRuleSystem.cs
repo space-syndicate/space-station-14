865c83da33bf335361403808995d7150c2db7f8f
_Svist_ <148935188+Svist666s@users.noreply.github.com>
2026-06-08 22:44:48 +0400

    ia ebanko

--- Content.Server/Corvax/GameTicking/Rules/EvacVoteRuleSystem.cs
+++ Content.Server/Corvax/GameTicking/Rules/EvacVoteRuleSystem.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 
 namespace Content.Server.Corvax.GameTicking.Rules;
 
-public partial class EvacVoteRuleSystem : GameRuleSystem<EvacVoteRuleComponent>
+public sealed partial class EvacVoteRuleSystem : GameRuleSystem<EvacVoteRuleComponent>
 {
     [Dependency] private IVoteManager _voteManager = default!;
     [Dependency] private ChatSystem _chat = default!;
