4a0f8f384e8354d3c51473be1dabc86412f74372
_Svist_ <148935188+Svist666s@users.noreply.github.com>
2026-09-23 15:40:59 +0400

    Фикс отправки целей станции (#3741)

--- Content.Server/GameTicking/ServerGameTicker.RoundFlow.cs
+++ Content.Server/GameTicking/ServerGameTicker.RoundFlow.cs
@@ -408,6 +408,7 @@ public sealed partial class ServerGameTicker
         AnnounceRound();
         UpdateInfoText();
         SendRoundStartedDiscordMessage();
+        RaiseLocalEvent(new RoundStartedEvent(RoundId)); // Corvax-StationGoal
 
 #if EXCEPTION_TOLERANCE
             }
