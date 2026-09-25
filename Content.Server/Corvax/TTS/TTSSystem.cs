00da8fe9326d96abad5ef7695e7fb107037bb166
Zekins3366 <zekins3366@gmail.com>
2026-09-25 23:42:32 +0300

    fixttstransmitwithoutchannels

--- Content.Server/Corvax/TTS/TTSSystem.cs
+++ Content.Server/Corvax/TTS/TTSSystem.cs
@@ -288,6 +288,9 @@ public sealed partial class TTSSystem : EntitySystem
         if (channelFlag == RadioChannelFlag.None)
             return; // Unknown - Skip
 
+        if (!CanTransmitFrom(sourceUid, channel))
+            return;
+
         var netSource = GetNetEntity(sourceUid);
         var ttsEvent = new PlayTTSEvent(soundData, netSource, isWhisper, true);
         var filter = Filter.Empty();
@@ -353,6 +356,20 @@ public sealed partial class TTSSystem : EntitySystem
             return;
 
         RaiseNetworkEvent(ttsEvent, filter, recordReplay: false);
+    }
+
+    private bool CanTransmitFrom(EntityUid uid, RadioChannelPrototype channel)
+    {
+        if (TryComp<IntrinsicRadioTransmitterComponent>(uid, out var transmitter))
+            return transmitter.Channels.Contains(channel.ID);
+
+        if (TryComp<WearingHeadsetComponent>(uid, out var wearing)
+            && TryComp<EncryptionKeyHolderComponent>(wearing.Headset, out var encryption))
+        {
+            return encryption.Channels.Contains(channel.ID);
+        }
+
+        return false;
     }
 
     /// <inheritdoc cref="TelecomServerComponent"/>
